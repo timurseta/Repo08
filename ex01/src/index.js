6a6f36a9bda1baf5c006b633e1528459c4b05cab
timurseta <timur_seta@hotmail.com>
2021-09-20 14:07:44 +0200

    added first function

---
+++ ex01/src/index.js
@@ -0,0 +1,10 @@
+function reusableFunction() {
+   
+    // Only write code below this line
+
+    // Only write code above this line
+
+}
+
+module.exports = reusableFunction;
+console.log("Hi Padawan!");
