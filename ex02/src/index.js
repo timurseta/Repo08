abc847383bf77a68e9ca216e56047a58c76ddf73
timurseta <timur_seta@hotmail.com>
2021-09-20 15:17:17 +0200

    added function

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -5,7 +5,10 @@ function functionPrintInfo(name, age){
     console.log(name, age);
 }
 
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
+
 // Write code above this line
 
-module.exports = functionPrintInfo("John", 23);
-module.exports = functionPrintInfo("Michael", 43);
+module.exports 
+module.exports
