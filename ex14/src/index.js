a2fce0714a1881e16fc9dd38a894b55d81014a0c
timurseta <timur_seta@hotmail.com>
2021-09-21 00:04:37 +0200

    string to number function used

---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+
+function convertStrToInt(str) {
+
+    var str = parseInt(str);
+    return str;
+}
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"));
+
+// Only change code above this line
+module.exports = convertStrToInt;
